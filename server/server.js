affdcff94af2628c4214f41a0c6ed10248b6973e
daniwinsss <dankdani06@gmail.com>
2025-06-27 08:19:05 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ await connectCloudinary()
 //allow multiple origins
 const allowedOrigins = ['http://localhost:5173']
 
-app.post('stripe',express.raw({type : 'application/json'}),stripeWebhooks)
+app.post('/stripe',express.raw({type : 'application/json'}),stripeWebhooks)
 // Middleware config
 app.use(express.json());
 app.use(cookieParser());
